63a205dc22853beed7a377ef70acbf4c82e75c37
Rendell Cale <rendellc@stud.ntnu.no>
2018-02-20 19:55:39 +0100

    IRIS omg

--- source_this_after_building.sh
+++ source_this_after_building.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#cp -r $(pwd)/models/* ~/.gazebo/models
-
 declare catkin_ws_dir="/home/$USER/Dev/catkin_ws"
 declare px4_dir="/home/$USER/Dev/PX4/PX4_2017"
 
@@ -10,10 +8,11 @@ declare px4_dir="/home/$USER/Dev/PX4/PX4_2017"
 # Get library files
 cp $catkin_ws_dir/devel/lib/libiarc_obstacle.so $catkin_ws_dir/src/control_obstacle_sim_gazebo/plugins
 
+source $px4_dir/Tools/setup_gazebo.bash $px4_dir $px4_dir/build_posix_sitl_default
 GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$catkin_ws_dir/src/control_obstacle_sim_gazebo/models
 GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$px4_dir/Tools/sitl_gazebo/models
 GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$catkin_ws_dir/src/control_obstacle_sim_gazebo/plugins
-GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$px4_dir/build_sitl_default/build_gazebo
+GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$px4_dir/build_posix_sitl_default/build_gazebo
 
 unset catkin_ws_dir
 unset px4_dir
