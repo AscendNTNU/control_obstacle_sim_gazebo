05e08c2b52779d9d73bbed7758882b79234779ff
Rendell Cale <rendellc@stud.ntnu.no>
2018-02-21 03:10:16 +0100

    Prettify the world slightly :heart_eyes:

--- source_this_after_building.sh
+++ source_this_after_building.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-declare catkin_ws_dir="../.."
+# NOTE: Needs to be absolute paths!
+declare catkin_ws_dir="/home/$USER/Dev/catkin_ws"
 declare px4_dir="/home/$USER/Dev/PX4/PX4_2017"
 
 #source '$(px4_dir)'/Tools/setup_gazebo.bash '$(px4_dir)' '$(px4_dir)'/build_posix_sitl_default
@@ -8,8 +9,8 @@ declare px4_dir="/home/$USER/Dev/PX4/PX4_2017"
 # Get library files
 cp $catkin_ws_dir/devel/lib/libiarc_obstacle.so $catkin_ws_dir/src/control_obstacle_sim_gazebo/plugins
 
-source $px4_dir/Tools/setup_gazebo.bash $px4_dir $px4_dir/build_posix_sitl_default
-GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$catkin_ws_dir/src/control_obstacle_sim_gazebo/models
+source $px4_dir/Tools/setup_gazebo.bash $px4_dir $px4_dir/build_posix_sitl_default/
+GAZEBO_MODEL_PATH=$catkin_ws_dir/src/control_obstacle_sim_gazebo/models:$GAZEBO_MODEL_PATH
 GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$px4_dir/Tools/sitl_gazebo/models
 GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$catkin_ws_dir/src/control_obstacle_sim_gazebo/plugins
 GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$px4_dir/build_posix_sitl_default/build_gazebo
