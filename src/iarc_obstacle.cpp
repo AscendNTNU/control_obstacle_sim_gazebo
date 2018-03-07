3d9bb127259b4d8b1066c9177f932b3fc3071bec
Rendell Cale <rendellc@stud.ntnu.no>
2018-03-07 12:31:51 +0100

    Change origin to better match iarc conditions

--- src/iarc_obstacle.cpp
+++ src/iarc_obstacle.cpp
@@ -3,6 +3,7 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/physics/Link.hh>
 #include <gazebo/common/common.hh>
+#include <sdf/sdf.hh>
 
 #include <stdio.h>
 
@@ -16,23 +17,33 @@ namespace gazebo
 
         public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
         {
-            printf("[custom] Model loaded!\n");
-            
+	    //this->startPose = _sdf->GetElement("iarc_obstacle")->GetValuePose();
+
+
+            const gazebo::math::Pose pose = _parent->GetWorldPose();
+
             this->model = _parent;
+
+	    this->start_pose = pose;
 
             this->updateConnection = event::Events::ConnectWorldUpdateBegin(
                     boost::bind(&Obstacle::OnUpdate, this, _1));
+
+            printf("[iarc_obstacle] Obstacle loaded at %.2f\t%.2f\n", 
+			    this->start_pose.pos.x, this->start_pose.pos.y);
+            
+
         }
 
         public: void OnUpdate(const common::UpdateInfo& _info)
         {
             constexpr auto vel{0.33};
             const gazebo::math::Pose pose = this->model->GetWorldPose();
-            const auto radius = std::sqrt(std::pow(pose.pos.x, 2) + std::pow(pose.pos.y, 2));
+            const auto radius = std::sqrt(std::pow(pose.pos.x - 10.0, 2) + std::pow(pose.pos.y - 10.0, 2));
             const auto angular_velocity = vel/radius;
 
-	    const auto vel_x = -angular_velocity*pose.pos.y;
-	    const auto vel_y =  angular_velocity*pose.pos.x;
+	    const auto vel_x = -angular_velocity*(pose.pos.y - 10.0);
+	    const auto vel_y =  angular_velocity*(pose.pos.x - 10.0);
 
             this->model->SetLinearVel(math::Vector3(vel_x, vel_y, 0));
 
@@ -41,6 +52,8 @@ namespace gazebo
         private: physics::ModelPtr model;
 
         private: event::ConnectionPtr updateConnection;
+
+	private: gazebo::math::Pose start_pose;
     };
 
     GZ_REGISTER_MODEL_PLUGIN(Obstacle)
